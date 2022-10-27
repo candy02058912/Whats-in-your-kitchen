573152b428b0bcb36d4122825bea7b8074f5e4f5
Candy Tsai <candy02058912@gmail.com>
2022-10-27 19:59:16 +0800

    Updates

---
+++ precache-manifest.dfee4608791c3b0053848c37c14f83f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32e2ff00290df4afb50f",
+    "url": "/static/css/main.3590dc58.chunk.css"
+  },
+  {
+    "revision": "32e2ff00290df4afb50f",
+    "url": "/static/js/main.32e2ff00.chunk.js"
+  },
+  {
+    "revision": "6b1cdae6b274c51b5452",
+    "url": "/static/js/1.6b1cdae6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a7fb4a15d409d78d8547b11f26c3673",
+    "url": "/static/media/pexels-lukas-616401.8a7fb4a1.jpg"
+  },
+  {
+    "revision": "ce660728c5e294030eb5c5f06c495f63",
+    "url": "/index.html"
+  }
+];
